7ed60d5cbaa698e5607ba67f607f5d28a2b199be
blackwellsmith <blackwellsmithiv@gmail.com>
2019-11-12 12:53:37 -0500

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let hearts = document.querySelectorAll(".like")
+
 
 // Your JavaScript code goes here!
+function likeClicked(event) {
+  console.log(event)
+  let heart = event.target
+  mimicServerCall().then(() => {
+    if (EMPTY_HEART === heart.innerText) {
+      heart.innerText = FULL_HEART
+      heart.classList.add("activated-heart")
+    } else {
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    }
+} )
+    .catch(function (error) {
+      document.getElementById("modal").className = "";
+      setTimeout(function () {
+        document.getElementById("modal").className = "hidden";
+      }, 5000);
+    })
+}
 
-
+for (let e of hearts) { e.addEventListener("click", likeClicked) }
 
 
 //------------------------------------------------------------------------------
