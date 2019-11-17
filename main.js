a68969a1b09038dcb18b8b1d12c58208d8578448
github username <github email address>
2019-11-16 22:34:00 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function() {
+  // Grab all like buttons
+  const heartBtn = document.querySelectorAll('span.like-glyph')
+  const modal = document.getElementById('modal')
 
+  // Add listener for like button click
+  for (select of heartBtn) {
+    select.addEventListener('click', function(e) {
+      // Set heart to what was clicked
+      let heart = e.target
+      
+      // Simulate server request
+      mimicServerCall()
+        .then(function() {
+          // If empty heart is clicked, change to full heart and update class
+          if (heart.innerText === EMPTY_HEART) {
+            heart.innerText = FULL_HEART 
+            heart.className = 'activated-heart'
+          } else {
+            // If heart is empty, ensure there is no class
+            heart.innerText = EMPTY_HEART 
+            heart.removeAttribute('class')
+          }
+        })
 
+        // If server returns a failure status
+        .catch(function(error) {
+          // Display error by removing hidden class
+          modal.removeAttribute('class')
+          // Display error message in the modal
+          modal.innerText = error
+          // Hide modal after 5 seconds
+          setTimeout(function() {
+            modal.className = 'hidden'
+          }, 5000)
+        })
+    })
+  }
+
+})
 
 
 //------------------------------------------------------------------------------
