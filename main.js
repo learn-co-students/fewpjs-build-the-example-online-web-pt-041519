dccda9ffce59f0872e983d739e6b84a0a5fd205c
earthctzn <earthctzn1@gmail.com>
2019-11-18 03:29:15 -0500

    Done.

--- main.js
+++ main.js
@@ -3,10 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorMssg = "Random server error. Try again."
+const successMssg = "Pretend remote server notified of action!"
+const hearts = document.getElementsByClassName("like-glyph")
+const err =  document.getElementById('modal')
+err.className = "hidden"
+
+const notify = (message, timeInteger) => {
+  err.className = ""
+  err.innerText = message
+  setTimeout(() =>{
+    err.className = "hidden"
+  }, parseInt(timeInteger))
+}
+
+const clkHeart = (click) => {
+  let heart = click.target
+  mimicServerCall().then(() => {
+    if (heart.innerText == EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className = "activated-heart"
+      notify(successMssg, '2000')
+    } else if (heart.innerText == FULL_HEART){
+      heart.innerText = EMPTY_HEART
+      heart.className = ""
+    } 
+  }).catch(() => {
+    notify(errorMssg, '5000')
+  })
+} 
 
 
-
-
+for (let heart of hearts){
+  heart.addEventListener("click", clkHeart)
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
@@ -16,10 +46,11 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject(errorMssg);
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve(successMssg);
       }
+
     }, 300);
   });
 }
