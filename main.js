edd1befc00b7b7988eb922c072e1b11bec040ab1
github username <github email address>
2019-11-19 07:22:23 -0500

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,52 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeSpan = document.querySelector('span.like-glyph')
+let articleHearts = document.querySelectorAll(".like");
+
+// to hide modal on page load 
+
+let modal = document.querySelector('div#modal')
+modal.className = 'hidden'
+
+// listen for heart click 
+let allHearts = document.querySelectorAll('.like')
+
+  for (let heart of allHearts) {
+  heart.addEventListener("click", likeCallBack);
+}
+
+// callback function for like 
+function likeCallBack() {
+  mimicServerCall().then(function(object) {
+    // change heart
+    changeHeart(event)
+  })
+  .catch(function(error) {
+    // Show modal
+    modal.classList = ""
+
+    // Insert error into modal
+    modal.querySelector('p#modal-message').innerText = error
+
+    // Hide modal after 5 seconds
+    setTimeout(function(){ modal.className = "hidden" }, 5000);
+  })
+}
+
+
+// Change heart between full and empty
+function changeHeart(event) {
+  // If empty heart, replace with full
+  if (event.target.innerHTML === EMPTY_HEART) {
+    event.target.innerHTML = FULL_HEART
+    event.target.className = "like-glyph activated-heart"
+  } else {
+    event.target.innerHTML = EMPTY_HEART
+    event.target.className = "like-glyph"
+  }
+}
+
 
 
 
